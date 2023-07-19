ba5eb3ef207ae972fcc761054be7fd2b5f512ed3
WorstUcdProgrammer <yizzhou@ucdavis.edu>
2023-07-18 20:46:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ const getPage = async (url) => {
   }
 };
 
+const getLink = async () => {
+  const html = await getPage(url);
+  if (html) {
+    const $ = cheerio.load(html);
+    const links = [];
+    $("[href]").each((index, element) => {
+      const link = $(element).attr("href");
+      if (link && !links.includes(link)) {
+        links.push(link);
+      }
+    });
+    console.log(links);
+  }
+};
+
 const url = "https://pacer.uscourts.gov/";
-const html = await getPage(url);
-if (html) {
-  const $ = cheerio.load(html);
-    
-}
+getLink(url);
